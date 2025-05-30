88f1cd5dba043b71b5015dc737f4c98f6a5014ce
svpontes <svpontes2015@gmail.com>
2025-05-31 00:14:40 +0100

    check on relative path

--- src/js/checkout.js
+++ src/js/checkout.js
@@ -1,5 +1,5 @@
 import { loadHeaderFooter } from "./utils.mjs";
-import { CheckoutProcess } from "./CheckoutProcess.mjs";
+import { CheckoutProcess } from "src\js\CheckoutProcess.mjs";//relative path
 
 loadHeaderFooter();
 
