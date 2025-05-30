4986663d0c82763e9ce6e2c64601c7e15c770061
svpontes <svpontes2015@gmail.com>
2025-05-31 00:17:41 +0100

    remove relative path

--- src/js/checkout.js
+++ src/js/checkout.js
@@ -1,5 +1,5 @@
 import { loadHeaderFooter } from "./utils.mjs";
-import { CheckoutProcess } from "srcjsCheckoutProcess.mjs"; //relative path
+import { CheckoutProcess } from ".CheckoutProcess.mjs";
 
 loadHeaderFooter();
 
